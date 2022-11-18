fb1c07ceeba4b1ae9e5336a4322a72778759eb5a
GabriielGoncalves <novais_gabriel@outlook.com>
2022-11-18 16:56:56 -0300

    refactors index.ts and adds error handling

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import 'reflect-metadata';
 import express from 'express';
 import bodyParser from 'body-parser';
 import { authRouter } from './routes/auth';
-import { DataSource } from 'typeorm';
+import { AppDataSource } from './models/database/data-source';
 
 const app = express();
 
@@ -12,24 +12,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', authRouter);
 
-const AppDataSource = new DataSource({
-    type: 'postgres',
-    host: process.env.DB_CONFIG_HOST,
-    port: process.env.DB_CONFIG_PORT as number | undefined,
-    username: process.env.DB_CONFIG_USERNAME,
-    password: process.env.DB_CONFIG_PASSWORD,
-    database: process.env.DB_CONFIG_DATABASE,
-    entities: ['./src/models/entity/*.ts'],
-    migrations: ['./src/models/migrations/*.ts'],
-});
-
-export const connection = AppDataSource.initialize();
-
-AppDataSource.initialize().then(() => {
-    console.log('Banco conectado');
-    app.listen(process.env.PORT || 3000, (): void => {
-        console.log(
-            `Server is running in the port ${process.env.PORT || 3000}`,
-        );
+AppDataSource.initialize()
+    .then(() => {
+        console.log('Banco conectado');
+        app.listen(process.env.PORT || 3000, (): void => {
+            console.log(
+                `Server is running in the port ${process.env.PORT || 3000}`,
+            );
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+        throw new Error(err);
     });
-});
