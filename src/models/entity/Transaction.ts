47a134e4cf30e960abb68d025bd06e6aa972a85f
GabriielGoncalves <novais_gabriel@outlook.com>
2022-11-21 18:09:51 -0300

    alter table transaction

--- src/models/entity/Transaction.ts
+++ src/models/entity/Transaction.ts
@@ -2,17 +2,25 @@ import {
     Column,
     CreateDateColumn,
     Entity,
+    ManyToOne,
     PrimaryGeneratedColumn,
 } from 'typeorm';
+import { Accounts } from './Account';
 
 @Entity('transactions')
 export class Transactions {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
-    @Column('decimal')
+    @Column({ type: 'double precision', default: 100.0 })
     value: number;
 
     @CreateDateColumn()
     createdAt: Date;
+
+    @ManyToOne(() => Accounts)
+    debitedAccount: Accounts;
+
+    @ManyToOne(() => Accounts)
+    creditedAccount: Accounts;
 }
